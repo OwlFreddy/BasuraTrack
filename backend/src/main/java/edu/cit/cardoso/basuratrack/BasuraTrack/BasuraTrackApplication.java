e61c34da449fa0a82dcbcf3ef929b4fe1e3b4c68
LAB22_USER ON WS29 <L22X16W29@LabHCI.net>
2026-07-01 16:31:06 +0800

    Springboot set up

---
+++ backend/src/main/java/edu/cit/cardoso/basuratrack/BasuraTrack/BasuraTrackApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.cardoso.basuratrack.BasuraTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasuraTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasuraTrackApplication.class, args);
+	}
+
+}
